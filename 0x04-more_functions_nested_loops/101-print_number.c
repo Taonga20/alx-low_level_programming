69f5a444a54c2b35e61ba6615d831f2b61999bf6
Taonga phiri <taongaphiri057@gmail.com>
2023-03-24 09:23:31 -0700

    print n

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,16 @@
  */
 void print_number(int n)
 {
-unsigned int 1 =  n;
+unsigned int i =  n;
 
 if (n < 0)
 {
 _putchar(45);
-1 = -1;
+i = -i;
 }
-if (1 / 10)
+if (i / 10)
 {
-print_number(1 / 10);
+print_number(i / 10);
 }
-_putchar(1 % 10 + '0');
+_putchar(i % 10 + '0');
 }
