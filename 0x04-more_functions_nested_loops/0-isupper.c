cb7d10892ad9b3c7ee00c6c649c7d572ee1cb745
Taonga phiri <taongaphiri057@gmail.com>
2023-03-24 06:40:38 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - upercase letters
+ * @C: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int C)
+{
+if (C  >= 'A' && C <= 'Z')
+return (1);
+else
+return (0);
+}
