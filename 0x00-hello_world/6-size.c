57786ed38adf0acab635b5b8392aec132af479d0
Taonga phiri <taongaphiri057@gmail.com>
2023-03-25 04:25:38 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-printf("Size of char: %zu  byte(s)\n", sizeof(char));
-printf("size of int: %zu byte(s)\n", sizeof(int));
-printf("size of long int: %zu byte(s)\n", sizeof(long int));
-printf("size of long long int: %zu byte(s)\n", sizeof(long long int));
-printf("size of float: %zu byte(s)\n", sizeof(float));
+printf("Size of a char: %1d  byte(s)\n", sizeof(char));
+printf("size of an int: %1d byte(s)\n", sizeof(int));
+printf("size of a long int: %1d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %1d byte(s)\n", sizeof(long long int));
+printf("size of a float: %1d byte(s)\n", sizeof(float));
 return (0);
 }
