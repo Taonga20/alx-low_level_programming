4ded0d9ab64707879399fb55a6e076ffbf302952
Taonga phiri <taongaphiri057@gmail.com>
2023-03-25 02:57:47 -0700

    six size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,15 @@
 /**
  * main - main function
  *
- * Return 0 (Success)
+ * Return: 0 on Success
  */
 
 int main(void)
 {
-printf("Size of char: %lu  byte(s)", sizeof(char));
-printf("size of int: %lu byte(s)", sizeof(int));
-printf("size of long int: %lu byte(s)", sizeof(long int));
-printf("size of long long int: %lu byte(s)", sizeof(long long int));
-printf("size of float: %lu byte(s)", sizeof(float));
+printf("Size of char: %lu  byte(s)\n", sizeof(char));
+printf("size of int: %lu byte(s)\n", sizeof(int));
+printf("size of long int: %lu byte(s)\n", sizeof(long int));
+printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+printf("size of float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
