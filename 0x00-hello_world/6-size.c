d6f9bac3342980e34ed6a7402b299e54d88e337c
Taonga phiri <taongaphiri057@gmail.com>
2023-03-25 03:15:34 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ printf("Size of char: %lu  byte(s)\n", sizeof(char));
 printf("size of int: %lu byte(s)\n", sizeof(int));
 printf("size of long int: %lu byte(s)\n", sizeof(long int));
 printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("size of float: %lu byte(s)\n", sizeof(float));
+printf("size of float: %lu byte(s)", sizeof(float));
 return (0);
 }
